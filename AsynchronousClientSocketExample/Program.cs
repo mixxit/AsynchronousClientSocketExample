b04ccaa7dc107d81153494167cfe6f8e8d49fbe9
Mixxit <mixxit@users.noreply.github.com>
2021-10-25 16:54:38 +0100

    example

---
+++ AsynchronousClientSocketExample/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Text;
+
+namespace AsynchronousClientSocketExample
+{
+    class Program
+    {
+        public static int Main(String[] args)
+        {
+            AsynchronousClient.StartClient();
+            return 0;
+        }
+    }
+}
